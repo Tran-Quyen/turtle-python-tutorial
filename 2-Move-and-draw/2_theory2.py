0fa1da9e874ac8954a327b45e04a0e70929424fe
Tran-Quyen <quyen10a8.sao@gmail.com>
2022-07-04 15:45:25 +0700

    #3 - Move and draw part 2

---
+++ 2-Move-and-draw/2_theory2.py
@@ -0,0 +1,78 @@
+# import turtle
+# from turtle import *
+
+'''
+- home()
+- circle()
+- dot()
+- stamp()
+- clearstamp()
+- clearstamps()
+- undo()
+- speed()
+'''
+
+# 1 - home() => (0,0)
+# fd(100)
+# lt(60)
+# fd(100)
+# home()
+
+# 2 - circle()
+# circle(50)
+# circle(120, 180)
+# circle(120, 360, 6)
+
+# 3 - dot()
+# fd(100)
+# dot(20)
+# dot(20, "blue")
+
+# 4 - stamp()
+# fd(100)
+# stamp()
+# lt(90)
+# fd(100)
+# stamp()
+# lt(90)
+# fd(100)
+# stamp()
+# lt(90)
+# fd(100)
+
+# 5 - clearstamp()
+# a = turtle.Turtle()
+# a.fd(100)
+# aStamp = a.stamp()
+# a.lt(90)
+# a.fd(100)
+# a.clearstamps(aStamp)
+
+# for i in range(8):
+#     a = turtle.stamp()
+#     turtle.fd(30)
+#     turtle.clearstamp(a)
+
+# 6 - clearstamps()
+# for i in range(8):
+#     a = turtle.stamp()
+#     turtle.fd(30)
+# turtle.clearstamps(3)
+
+# 7 - undo()
+# for i in range(4):
+#     turtle.fd(50)
+#     turtle.lt(80)
+
+# for i in range(8):
+#     turtle.undo()
+
+# 8 - speed()
+# turtle.speed(5)
+# turtle.speed('slowest')
+
+# for i in range(4):
+#     turtle.fd(50)
+#     turtle.lt(80)
+
+# turtle.done()
