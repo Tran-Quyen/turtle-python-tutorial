0790278d199b1734081d3e2715aa92faf970a8ac
Tran-Quyen <quyen10a8.sao@gmail.com>
2022-07-04 15:45:02 +0700

    #2 - Move and draw part 1

---
+++ 2-Move-and-draw/1_theory1.py
@@ -0,0 +1,43 @@
+# import turtle
+# from turtle import *
+
+# Ctrl + ?
+
+# 1 - forward() | fd()
+# forward(100)
+# fd(100)
+
+# 2 - backward() | bk() | back()
+# bk(200)
+
+# 3 - right() | rt() angle
+# angle: 0 - 360
+# fd(100)
+# rt(90)
+# fd(100)
+
+# 4 - left() | lt()
+# lt(90)
+
+# 5 - goto() | setpos() | setposition()
+# fd(100)
+# goto(100, 50)
+# setpos(100, 50)
+# setposition(100, 50)
+
+# 6 - setx() => (x,0)
+# fd(100)
+# setx(-200)
+
+# 7 - sety() => (0,y)
+# fd(100)
+# sety(-100)
+
+# 8 - setheading() | seth()
+# fd(100)
+# seth(90)
+# lt(90)
+# rt(90)
+# fd(100)
+
+# done()
---
+++ 2-Move-and-draw/README.md
@@ -0,0 +1,24 @@
+# Turtle methods
+
+## Turtle motion
+
+#### Move and draw
+
+- forward() | fd()
+- backward() | bk() | back()
+- right() | rt()
+- left() | lt()
+- goto() | setpos() | setposition()
+- setx()
+- sety()
+- setheading() | seth()
+- home()
+- circle()
+- dot()
+- stamp()
+- clearstamp()
+- clearstamps()
+- undo()
+- speed()
+
+
