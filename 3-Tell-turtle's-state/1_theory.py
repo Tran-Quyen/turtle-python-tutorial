4e5d15952699fb1d08df60ca63cb4e38e1ad1011
Tran-Quyen <quyen10a8.sao@gmail.com>
2022-07-11 01:30:25 +0700

    \#5 - Turtle state, measurement

---
+++ 3-Tell-turtle's-state/1_theory.py
@@ -0,0 +1,68 @@
+# from turtle import *
+# Tell Turtle’s state
+
+# position() | pos()
+# setpos(200, 300)
+# fd(200)
+# print(pos())
+
+# towards()
+# fd(100)
+# print(towards(0, 100))
+# print(towards(0, -100))
+
+# xcor()
+# home()
+# left(50)
+# forward(100)
+# print(pos())
+
+# print(round(xcor(), 5))
+
+# ycor()
+# home()
+# left(50)
+# forward(100)
+# print(pos())
+
+# print(round(ycor(), 2))
+
+# heading()
+# home()
+# fd(100)
+# lt(67)
+# print(heading())
+
+# distance()
+# from turtle import *
+
+# a = Turtle()
+# a.home()
+# print(a.distance((30, 40)))
+
+# b = Turtle()
+# b.forward(77)
+# print(a.distance(b))
+
+# Setting and measurement
+
+# degrees()
+# home()
+# left(90)
+# print(heading())
+
+# print(degrees(400))
+# print(heading())
+
+# print(degrees(360))
+# print(heading())
+
+# radians()
+# home()
+# left(90)
+# print(heading())
+
+# radians()
+# print(heading())
+
+# done()
