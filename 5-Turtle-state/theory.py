958485de87131be7c4c47f449bd71cbe20602952
Tran-Quyen <quyen10a8.sao@gmail.com>
2022-07-20 00:47:51 +0700

    #8 - Turtle state

---
+++ 5-Turtle-state/theory.py
@@ -0,0 +1,37 @@
+# from turtle import *
+# Visibility
+
+# - showturtle() | st()
+# - hideturtle() | ht()
+# - isvisible()
+# speed('slow')
+# print(isvisible())
+# hideturtle()
+# print(isvisible())
+# forward(100)
+# left(90)
+# showturtle()
+# forward(100)
+# print(isvisible())
+
+# Appearance
+
+# - shape()
+# - resizemode()
+# - shapesize() | turtlesize()
+# - shearfactor() => cut
+# - settiltangle() - tiltangle() - tilt() => rotate
+# - shapetransform()
+# - get_shapepoly()
+# resizemode('user')
+# shearfactor(0.5)
+# tilt(30)
+# color('green')
+# shape('turtle')  # arrow, circle, square, ...
+# shapesize(3)
+
+# shearfactor(-0.5)
+# shapetransform(4, -1, 0, 2)
+# print(get_shapepoly())
+
+# done()
